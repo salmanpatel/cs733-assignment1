c0a9b9daf72825a96813963048963a74a97c93f3
salmanpatel <salmanpatel131@gmail.com>
2016-01-20 23:23:39 +1100

    expiration time mechanism handeled

--- server.go
+++ server.go
@@ -10,15 +10,14 @@ import (
 	"strconv"
 	"io"
 	"time"
+	"math"
 )
 
 type File struct {
-//	fileLock sync.RWMutex
 	fileName string
 	fileSize uint64
 	fileVersionNo uint64
 	fileExpTime uint64
-	fileExpTimeBool bool
 	fileContent []byte
 }
 
@@ -29,11 +28,75 @@ var fileSystem map[string]File
 func writeFile(conn net.Conn ,cmdTokens []string) {
 	fmt.Println("Inside write file")
 	conn.Write([]byte(""))
-//	var buffer string
-//	buffer,_ = bufio.NewReader(conn).ReadString('\n')
-//	fmt.Println(buffer)
 	fileSize, err := strconv.ParseUint(cmdTokens[2],10,64)
 	if err != nil {
+		conn.Write([]byte("ERR_CMD_ERR\r\n"))
+		conn.Close()
+		return
+	}
+	var contentBuffer []byte = make([]byte,fileSize)
+	n, err := io.ReadFull(conn, contentBuffer)
+//	fmt.Println(contentBuffer)
+//	fmt.Println(n)
+	if err != nil || uint64(n) < fileSize {
+		conn.Write([]byte("ERR_INTERNAL\r\n"))
+		conn.Close()
+		return
+	}
+	// Reading and discarding "\r\n"
+	bufio.NewReader(conn).ReadByte()
+//	bufio.NewReader(conn).ReadByte()
+//	contentBuffer = nil
+	var expTimeUser uint64
+	var expTime uint64 = math.MaxUint64
+	if(len(cmdTokens)==4) {
+		expTimeUser, err = strconv.ParseUint(cmdTokens[3],10,64)
+		if err != nil {
+			conn.Write([]byte("ERR_CMD_ERR\r\n"))
+			return
+		}
+		expTime = psuedoTime + expTimeUser
+	}
+	var versionNo uint64 = initialVersionNo
+	if fileObj, ok := fileSystem[cmdTokens[1]]; ok  {
+//	if _, ok := fileSystem[cmdTokens[1]]; ok  {
+		// present in dict
+		fileObj.fileContent = contentBuffer
+		fileObj.fileVersionNo += 1
+		fileObj.fileSize = fileSize
+		fileObj.fileExpTime = expTime
+		fileSystem[cmdTokens[1]] = fileObj
+		versionNo = fileObj.fileVersionNo
+	} else {
+		fileSystem[cmdTokens[1]] = File{cmdTokens[1],fileSize, initialVersionNo, expTime, contentBuffer}
+	}
+//	conn.Write([]byte("OK " + fileObj.fileVersionNo + "\r\n"))
+	conn.Write([]byte("OK " + strconv.FormatUint(versionNo,10) + "\r\n"))
+}
+
+func readFile(conn net.Conn, cmdTokens []string) {
+	fmt.Println("Inside read file")
+	if fileObj, ok := fileSystem[cmdTokens[1]]; !ok || fileObj.fileExpTime < psuedoTime {
+		conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
+		return
+        } else {
+		expTime := ""
+		if fileObj.fileExpTime != math.MaxUint64 {
+			fmt.Println("psuedoTime" + strconv.FormatUint(psuedoTime,10))
+			fmt.Println("fileObj.fileExpTime" + strconv.FormatUint(fileObj.fileExpTime,10))
+			expTime = " " + strconv.FormatUint(fileObj.fileExpTime-psuedoTime,10)
+		}
+		returnVal := "CONTENTS " + strconv.FormatUint(fileObj.fileVersionNo, 10) + " " + strconv.FormatUint(fileObj.fileSize, 10) + expTime + "\r\n"
+		returnVal2 := append([]byte(returnVal), fileObj.fileContent...)
+		conn.Write(append(returnVal2, []byte("\r\n")...))
+	}
+}
+
+func casFile(conn net.Conn, cmdTokens []string) {
+	fmt.Println("Inside CAS file")
+	conn.Write([]byte(""))
+	fileSize, err1 := strconv.ParseUint(cmdTokens[3],10,64)
+	if err1 != nil  {
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		conn.Close()
 		return
@@ -49,99 +112,36 @@ func writeFile(conn net.Conn ,cmdTokens []string) {
 	}
 	// Reading and discarding "\r\n"
 	bufio.NewReader(conn).ReadByte()
-//	bufio.NewReader(conn).ReadByte()
-//	contentBuffer = nil
-	expTimeBool := false
-	var expTime uint64
-	if(len(cmdTokens)==4) {
-		expTime, err = strconv.ParseUint(cmdTokens[2],10,64)
-		if err != nil {
+	versionNo, err2 := strconv.ParseUint(cmdTokens[2],10,64)
+	if err2 != nil {
+		conn.Write([]byte("ERR_CMD_ERR\r\n"))
+		return
+	}
+	var expTimeUser uint64
+	var err3 error
+	var expTime uint64 = math.MaxUint64
+	if(len(cmdTokens)==5) {
+		expTimeUser, err3 = strconv.ParseUint(cmdTokens[4],10,64)
+		if err3 != nil {
 			conn.Write([]byte("ERR_CMD_ERR\r\n"))
-			conn.Close()
 			return
 		}
-		expTimeBool = true
-	}
-	var versionNo uint64 = initialVersionNo
-	if fileObj, ok := fileSystem[cmdTokens[1]]; ok  {
-//	if _, ok := fileSystem[cmdTokens[1]]; ok  {
-		// present in dict
-		fileObj.fileContent = contentBuffer
-		fileObj.fileVersionNo += 1
-		fileObj.fileSize = fileSize
-		fileSystem[cmdTokens[1]] = fileObj
-		versionNo = fileObj.fileVersionNo
-	} else {
-		fileSystem[cmdTokens[1]] = File{cmdTokens[1],fileSize, initialVersionNo, expTime, expTimeBool, contentBuffer}
-	}
-//	conn.Write([]byte("OK " + fileObj.fileVersionNo + "\r\n"))
-	conn.Write([]byte("OK " + strconv.FormatUint(versionNo,10) + "\r\n"))
-}
-
-func readFile(conn net.Conn, cmdTokens []string) {
-	fmt.Println("Inside read file")
-	if fileObj, ok := fileSystem[cmdTokens[1]]; !ok  {
-		conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
-		return
-        } else {
-		expTime := ""
-		if fileObj.fileExpTimeBool {
-			expTime = " " + strconv.FormatUint(fileObj.fileExpTime,10)
-		}
-		returnVal := "CONTENTS " + strconv.FormatUint(fileObj.fileVersionNo, 10) + " " + strconv.FormatUint(fileObj.fileSize, 10) + expTime + "\r\n"
-		returnVal2 := append([]byte(returnVal), fileObj.fileContent...)
-		conn.Write(append(returnVal2, []byte("\r\n")...))
-	}
-}
-
-func casFile(conn net.Conn, cmdTokens []string) {
-	fmt.Println("Inside CAS file")
-	conn.Write([]byte(""))
-	versionNo, err1 := strconv.ParseUint(cmdTokens[2],10,64)
-	fileSize, err2 := strconv.ParseUint(cmdTokens[3],10,64)
-	var expTimeBool bool
-	var expTime uint64
-	var err3 error
-	if(len(cmdTokens)==5) {
-		expTimeBool = true
-		expTime, err3 = strconv.ParseUint(cmdTokens[4],10,64)
-	}
-	if err1 != nil || err2 != nil || err3 != nil {
-		conn.Write([]byte("ERR_CMD_ERR\r\n"))
-		conn.Close()
-		return
+		expTime = psuedoTime + expTimeUser
 	}
 	var fileObj File
 	var ok bool
 	if fileObj, ok = fileSystem[cmdTokens[1]]; !ok  {
 		conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
-		// For now, I am considering this error as not recoverable
-		conn.Close()
 		return
         } else {
 		if versionNo != fileObj.fileVersionNo {
 			conn.Write([]byte("ERR_VERSION\r\n"))
-			// For now, I am considering this error as not recoverable
-			conn.Close()
 			return
 		}
-		var contentBuffer []byte = make([]byte,fileSize)
-		n, err := io.ReadFull(conn, contentBuffer)
-		fmt.Println(contentBuffer)
-		fmt.Println(n)
-		if err != nil || uint64(n) < fileSize {
-			conn.Write([]byte("ERR_INTERNAL\r\n"))
-			conn.Close()
-			return
-		}
-		// Reading and discarding "\r\n"
-		bufio.NewReader(conn).ReadByte()
 		fileObj.fileVersionNo += 1
 		fileObj.fileContent = contentBuffer
 		fileObj.fileSize = fileSize
-		if expTimeBool {
-			fileObj.fileExpTime = expTime
-		}
+		fileObj.fileExpTime = expTime
 		fileSystem[cmdTokens[1]] = fileObj
 	        conn.Write([]byte("OK " + strconv.FormatUint(fileSystem[cmdTokens[1]].fileVersionNo,10) + "\r\n"))
 	}
